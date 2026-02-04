e4a7a8cdf1e1c745238e1f2cfa4b9909c900a0a3
jhilde8 <hildebrand28@outlook.com>
2026-02-04 15:48:10 -0500

    bnl shell script update

--- shells/cmd-run-bnl.sh
+++ shells/cmd-run-bnl.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=psrc_meson
 #SBATCH --output=psrc_meson-%j.out
 #SBATCH --error=psrc_meson-%j.err
-#SBATCH --time=02:00:00
-#SBATCH --nodes=4
-#SBATCH --ntasks=4
+#SBATCH --time=12:00:00
+#SBATCH --nodes=8
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=48
 #SBATCH --partition=lqcd
 #SBATCH --account=class-c-2pipsrc
@@ -21,6 +21,6 @@ rm -r /hpcgpfs01/work/lqcd/staging/RBC/qcddata/MDWF/2+1f/48nt96/IWASAKI/b2.13/ls
 # mpiexec -n 2 --oversubscribe --bind-to none python3 -m mpi4py /home/frank/Qlattice-cc/examples-py-gpt/gpt-qlat-data-gen-pipi-qed.py --mpi 1.1.1.2 --mpi 1.1.2 >log.full.txt 2>&1
 # mpiexec -n 4 --oversubscribe --bind-to none python3 -m mpi4py /home/frank/Qlattice-cc/examples-py-gpt/gpt-qlat-data-gen-pipi-qed.py --mpi 1.1.2.2 --mpi 1.2.2 --job_tag_list "48I" --no-inversion >log.full.txt 2>&1
 
-srun -n 4 python3 -m mpi4py /sdcc/u/jhildebra/Ktopipi-gpt-qlat/scripts/gpt-qlat-meson-corr.py --mpi 1.1.2.2 --mpi 1.2.2 --job_tag_list "48I" --no-inversion > log.${SLURM_JOB_ID}.txt 2>&1
+srun -n 8 python3 -m mpi4py /sdcc/u/jhildebra/Ktopipi-gpt-qlat/scripts/gpt-qlat-pipisc.py --mpi 1.1.2.4 --mpi 2.2.2 --job_tag_list "48I" --no-inversion > log.${SLURM_JOB_ID}.txt 2>&1
 
 
